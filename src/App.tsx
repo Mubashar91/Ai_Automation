5fd1bdac26cce1d85e2b96062cb844bb1be67938
Mubashar91 <114853105+Mubashar91@users.noreply.github.com>
2026-02-13 00:23:53 +0500

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { HeroSection } from '@/components/hero'
 import { BenefitsSection } from '@/components/benefits'
 import { ChallengesSection } from '@/components/challenges'
 import { PillarsSection } from '@/components/pillars'
-// import { ProcessSection } from '@/components/process'
+import { ProcessSection } from '@/components/process'
 import { AutomationBenefitsSection } from '@/components/automation-benefits'
 import { ToolsSection } from '@/components/tools'
 import { PackagesSection } from '@/components/packages'
 import { FAQSection } from '@/components/faq'
-// import { CTASection, QuestionCTA } from '@/components/cta'
+import { CTASection, QuestionCTA } from '@/components/cta'
 import { Footer } from '@/components/footer'
 
 function App() {
@@ -20,23 +20,20 @@ function App() {
         <main className="bg-background transition-colors duration-300 w-full">
           <Header />
           <HeroSection />
-            <BenefitsSection />
-               <ChallengesSection />
-                 <PillarsSection />
-       
-     
+      
+        <QuestionCTA question="Find out how you can cut costs?" />
+        <BenefitsSection />
+        <ChallengesSection />
+        <QuestionCTA question="How can I scale without raising costs?" />
+        <PillarsSection />
+        <ProcessSection />
         <AutomationBenefitsSection />
-         <ToolsSection />
-         <FAQSection />
-            <PackagesSection />
-         
-          <Footer />
-        {/*
-     
+        <ToolsSection />
+        <PackagesSection />
         <QuestionCTA question="Where are hidden inefficiencies costing you money right now?" />
-        
-        
-          <Footer /> */} 
+        <FAQSection />
+          <CTASection />
+          <Footer />
         </main>
       </div>
     </Providers>
